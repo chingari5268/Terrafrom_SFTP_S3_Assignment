b32ec9681a0c2a0bf640233d4b6e301703369b4f
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-08 19:28:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ variable "agencies" {
 resource "aws_s3_bucket" "agency_bucket" {
   count  = length(var.agencies)
   bucket = "${var.agencies[count.index]}-bucket"
-  tags   = {
-    Name = "${var.agencies[count.index]}-bucket"
+  tags = {
+    Name = "${var.agencies}-bucket"
   }
 }
 
@@ -27,6 +27,48 @@ resource "aws_s3_bucket_acl" "agency_bucket_acl" {
   acl = "private"
 }
 
+# Add lifecycle policy to move data to glacier after 90 days
+resource "aws_s3_bucket_lifecycle_configuration" "agency_bucket_lifecycle" {
+  count  = length(var.agencies)
+  bucket = aws_s3_bucket.agency_bucket[count.index].id
+
+  rule {
+    id      = "move-to-glacier"
+    status  = "Enabled"
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+
+    filter {
+      prefix = "/"
+    }
+  }
+}
+
+
+# Enable SSE for each S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "agency_bucket_sse" {
+  count = length(var.agencies)
+  bucket = aws_s3_bucket.agency_bucket[count.index].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Enable versioning for each S3 bucket
+resource "aws_s3_bucket_versioning" "agency_bucket_versioning" {
+  count = length(var.agencies)
+  bucket = aws_s3_bucket.agency_bucket[count.index].id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 # Create the IAM roles and policies for each agency
 resource "aws_iam_role" "agency_role" {
