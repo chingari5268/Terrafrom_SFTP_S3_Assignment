ab65c1871eef6696e7340435d3ceb8333c8f3d8b
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-08 18:12:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,48 +3,41 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# Define the agency name
-variable "agency" {
-  type    = string
-  default = "agency-a"
+# Define the agency names
+variable "agencies" {
+  type    = list(string)
+  default = ["agency-a", "agency-b"]
 }
 
 # Declare the variable for the SSH key credential
 variable "ssh_key_file_content" {
-  type    = string
+  type      = string
   sensitive = true
 }
 
-# Create the SFTP server
-resource "aws_transfer_server" "sftp" {
-  identity_provider_type = "SERVICE_MANAGED"
-  protocols              = ["SFTP"]
-  endpoint_type          = "PUBLIC"
-  tags = {
-    Name = "sftp-server"
-  }
-}
-
-# Create the S3 bucket for the agency
+# Create the S3 bucket for each agency
 resource "aws_s3_bucket" "agency_bucket" {
-  bucket = "${var.agency}-bucket"
-  tags = {
-    Name = "${var.agency}-bucket"
+  count  = length(var.agencies)
+  bucket = "${var.agencies[count.index]}-bucket"
+  tags   = {
+    Name = "${var.agencies[count.index]}-bucket"
   }
 }
 
-# Set the ACL for the S3 bucket
+# Set the ACL for each S3 bucket
 resource "aws_s3_bucket_acl" "agency_bucket_acl" {
-  bucket = aws_s3_bucket.agency_bucket.id
+  count  = length(var.agencies)
+  bucket = aws_s3_bucket.agency_bucket[count.index].id
 
-  # Set the ACL to private
-  # See https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl for more options
+  # Set the ACL to private and restrict file types
   acl = "private"
 }
 
-# Create the IAM role for the agency
+
+# Create the IAM roles and policies for each agency
 resource "aws_iam_role" "agency_role" {
-  name  = "${var.agency}-role"
+  count = length(var.agencies)
+  name  = "${var.agencies[count.index]}-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -61,9 +54,10 @@ resource "aws_iam_role" "agency_role" {
 
 # Create the IAM policy for the agency
 resource "aws_iam_policy" "agency_policy" {
-  name  = "${var.agency}-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
+  count  = length(var.agencies)
+  name   = "${var.agencies[count.index]}-policy"
+   policy = jsonencode({
+    Version   = "2012-10-17"
     Statement = [
       {
         Action   = [
@@ -72,8 +66,25 @@ resource "aws_iam_policy" "agency_policy" {
         ]
         Effect   = "Allow"
         Resource = [
-          "${aws_s3_bucket.agency_bucket.arn}/*"
+          "${aws_s3_bucket.agency_bucket[count.index].arn}/*"
         ]
+        Condition = {
+          "StringEquals": {
+            "s3:x-amz-meta-filetype": [
+              "csv",
+              "excel",
+              "json"
+            ]
+          }
+          "NumericLessThanEquals": {
+            "s3:content-length": 52428800 # 50 MB
+          }
+        },
+        Principal = {
+          AWS = [
+            aws_transfer_user.sftp_user[count.index].arn
+          ]
+        }
       }
     ]
   })
@@ -81,55 +92,99 @@ resource "aws_iam_policy" "agency_policy" {
 
 # Attach the IAM policy to the IAM role for the agency
 resource "aws_iam_role_policy_attachment" "agency_policy_attachment" {
-  policy_arn      = aws_iam_policy.agency_policy.arn
-  role            = aws_iam_role.agency_role.name
+  count           = length(var.agencies)
+  policy_arn      = aws_iam_policy.agency_policy[count.index].arn
+  role            = aws_iam_role.agency_role[count.index].name
 }
 
-
-# Create an SSH key for the SFTP user
-resource "aws_transfer_ssh_key" "sftp_user_ssh_key" {
-  server_id = aws_transfer_server.sftp.id
-  user_name = aws_transfer_user.sftp_user.user_name
-  body      = var.ssh_key_file_content
+# Create the SFTP server and users for each agency
+resource "aws_transfer_server" "sftp" {
+  count                 = length(var.agencies)
+  identity_provider_type = "SERVICE_MANAGED"
+  protocols              = ["SFTP"]
+  endpoint_type          = "PUBLIC"
+  tags = {
+    Name = "${var.agencies[count.index]}-sftp-server"
+  }
 }
 
-# Configure the SFTP user with the SSH key
+# Generate an RSA key pair for each agency user
+resource "tls_private_key" "sftp_key" {
+  count = length(var.agencies)
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Upload the public key to the SFTP server for each agency user
+resource "aws_transfer_ssh_key" "sftp_ssh_key" {
+  count     = length(var.agencies)
+  server_id = aws_transfer_server.sftp[count.index].id
+  user_name = "${var.agencies[count.index]}-user"
+  body      = tls_private_key.sftp_key[count.index].public_key_openssh
+}
+
+# Store the private key securely in AWS Secrets Manager for each agency user
+resource "aws_kms_key" "sftp_key_kms" {
+  count      = length(var.agencies)
+  description = "KMS key for ${var.agencies[count.index]} SFTP private key"
+  enable_key_rotation = true
+}
+
+resource "aws_secretsmanager_secret" "sftp_key_secret" {
+  count       = length(var.agencies)
+  name        = "${var.agencies[count.index]}-sftp-key-secret"
+  kms_key_id  = aws_kms_key.sftp_key_kms[count.index].arn
+}
+
+resource "aws_secretsmanager_secret_version" "sftp_key_secret_version" {
+  count       = length(var.agencies)
+  secret_id   = aws_secretsmanager_secret.sftp_key_secret[count.index].id
+  secret_string = tls_private_key.sftp_key[count.index].private_key_pem
+}
+
+# Create an SFTP user for each agency with public key authentication
 resource "aws_transfer_user" "sftp_user" {
-  server_id          = aws_transfer_server.sftp.id
-  user_name          = "${var.agency}-user"
-  home_directory     = "/${var.agency}-bucket"
-  home_directory_type = "LOGICAL"
-  role               = aws_iam_role.agency_role.arn
+  count           = length(var.agencies)
+  server_id       = aws_transfer_server.sftp[count.index].id
+  user_name       = "${var.agencies[count.index]}-user"
+  home_directory  = "/${var.agencies[count.index]}-bucket"
+  role            =  aws_iam_role.agency_role[count.index].arn
+
+  tags = {
+    Name = "${var.agencies[count.index]}-sftp-user"
+  }
 }
 
 # Output the values required to connect the SFTP user to the server
 output "agency_sftp_server_id" {
-  value = aws_transfer_server.sftp.id
+  value = [for i in range(length(var.agencies)):
+             aws_transfer_server.sftp[i].id
+          ]
 }
 
+# Print the endpoint URL of the SFTP server for each agency
 output "agency_sftp_server_url" {
-  value = aws_transfer_server.sftp.endpoint
-}
-
-output "login_command" {
-  value = "sftp -i /path/to/key.pem ${aws_transfer_user.sftp_user.user_name}@${aws_transfer_server.sftp.endpoint}"
+  value = [for i in range(length(var.agencies)) :
+             aws_transfer_server.sftp[i].endpoint
+  ]
 }
 
 # Configure the CloudWatch metric alarm to monitor the S3 bucket for each agency
 resource "aws_cloudwatch_metric_alarm" "missing_data_alarm" {
-  alarm_name      = "${var.agency}-missing-data-alarm"
+  count            = length(var.agencies)
+  alarm_name       = "${var.agencies[count.index]}-missing-data-alarm"
   comparison_operator = "LessThanThreshold"
   evaluation_periods = 1
-  metric_name     = "NumberOfObjects"
-  namespace       = "AWS/S3"
-  period          = 300 # for every 5 minutes
-  statistic       = "Average"
-  threshold       = 1
-  alarm_description = "Alert if the number of objects in the S3 bucket for ${var.agency} is less than expected"
-  alarm_actions   = [aws_sns_topic.incident_alerts.arn] # Replace with your SNS topic ARN for email notifications
+  metric_name      = "NumberOfObjects"
+  namespace        = "AWS/S3"
+  period           = 300 # for every 5 minutes
+  statistic        = "Average"
+  threshold        = 1
+  alarm_description = "Alert if the number of objects in the S3 bucket for ${var.agencies[count.index]} is less than expected"
+  alarm_actions    = [aws_sns_topic.incident_alerts.arn] # Replace with your SNS topic ARN for email notifications
 
   dimensions = {
-    BucketName = aws_s3_bucket.agency_bucket.id
+    BucketName = aws_s3_bucket.agency_bucket[count.index].id
   }
 }
 
@@ -142,6 +197,5 @@ resource "aws_sns_topic" "incident_alerts" {
 resource "aws_sns_topic_subscription" "sre_email_subscription" {
   topic_arn = aws_sns_topic.incident_alerts.arn
   protocol  = "email"
-  endpoint  = "chethan7119982@gmail.com"
+  endpoint  = "sre-team@example.com"
 }
-
