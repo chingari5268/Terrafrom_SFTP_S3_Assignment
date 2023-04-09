9f4baf809fe4bd92169cf739fdb770026d5e5f51
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-09 09:30:33 +0530

    Delete modules/s3 directory

--- modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output the S3 bucket ARN
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.agency_bucket.arn
-}
-
-# Output the S3 bucket name
-output "s3_bucket_name" {
-  value = aws_s3_bucket.agency_bucket.id
-}
