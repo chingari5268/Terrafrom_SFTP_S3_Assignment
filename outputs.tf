ea79db9cb384151db8b3d3e9ace5db1bbbabf452
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-09 20:23:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,28 @@
 # Output the Bucket ID of each S3 bucket
 output "s3_buckets" {
-  value = aws_s3_bucket.agency_bucket.*.id
+  value = aws_s3_bucket.agency_bucket.id
 }
 
 # Output the Bucket name of each S3 bucket
 output "s3_bucket_names" {
-  value = aws_s3_bucket.agency_bucket.*.bucket
+  value = aws_s3_bucket.agency_bucket.bucket
 }
 
-# Output the ARN of each IAM role
-output "agency_role_arns" {
-  value = [
-    for i in range(length(var.agencies)) :
-    aws_iam_role.agency_role[i].arn
-  ]
+output "agency_iam_role_arn" {
+  value = aws_iam_role.agency_role.arn
 }
 
-# Output the ARN of each IAM policy
-output "agency_policy_arns" {
-  value = [
-    for i in range(length(var.agencies)) :
-    aws_iam_policy.agency_policy[i].arn
-  ]
+output "agency_iam_policy_arn" {
+  value = aws_iam_policy.agency_policy.arn
 }
+
 
 # Output the values required to connect the SFTP user to the server
 output "agency_sftp_server_id" {
-  value = [for i in range(length(var.agencies)):
-             aws_transfer_server.sftp[i].id
-          ]
+  value = aws_transfer_server.sftp.id
 }
 
-# Print the endpoint URL of the SFTP server for each agency
+# output the endpoint URL of the SFTP server for agency
 output "agency_sftp_server_url" {
-  value = [for i in range(length(var.agencies)) :
-             aws_transfer_server.sftp[i].endpoint
-  ]
+  value = aws_transfer_server.sftp.endpoint
 }
