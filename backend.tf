180b90d004e62dde403923d7535d126a1b0e7224
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-09 21:26:44 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "sftpterraformstate"
+    key = "prod/aws_infra"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
+}
