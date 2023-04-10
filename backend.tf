ee7f98ffb1662956f3d17ff0d9b288a81d13901a
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-10 19:52:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sftpterraformstatefile"
+    bucket = "sftpterraformstatefilelock"
     key = "prod/aws_infra"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"
