3fcfdc6bab8c01604456c942173d90ccf094971f
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-11 10:48:07 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sftpterraformstatefilelock"
-    key = "prod/aws_infra"
-    region = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt = true
-  }
-}
