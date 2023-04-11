8c9952fd26a29505faa0f9840c4d54060cd95cf8
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-11 19:17:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,12 +100,9 @@ resource "aws_transfer_server" "sftp" {
 resource "aws_transfer_user" "sftp_user" {
   server_id       = aws_transfer_server.sftp.id
   user_name       = "${var.agencies}-user"
+  home_directory  = "/${aws_s3_bucket.agency_bucket.bucket}"
   role            = aws_iam_role.agency_role.arn
-  home_directory_type    = "LOGICAL"
-  home_directory_mappings {
-    entry  = "/${var.agencies}-bucket"
-    target = "/${aws_s3_bucket.agency_bucket.id}/sftp/${var.agencies}/$${Transfer:user_name}"
-}
+
   # Set the IAM policy for the user to allow file uploads to S3
   policy = jsonencode({
     Version   = "2012-10-17"
